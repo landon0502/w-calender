d5f33779998f0a2bbf05f8fa1bfd71186bd17599
wang huan <whuan0322@163.com>
2025-01-06 09:51:28 +0800

    test:  Adjust the test script

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -2,21 +2,21 @@ import { defineWorkspace } from 'vitest/config';
 
 export default defineWorkspace([
   'packages/*',
-  {
-    extends: './packages/cli/src/configs/vite.calender.base.ts',
-    test: {
-      // ...
-      include: ['tests/**/*.{browser}.test.{ts,js}'],
-      // 在使用内联配置的时候，建议定义一个名称
-      name: 'w-calender-test',
-      environment: 'w-calender-test',
-    },
-  },
-  {
-    test: {
-      include: ['tests/**/*.{node}.test.{ts,js}'],
-      name: 'node',
-      environment: 'node',
-    },
-  },
+  // {
+  //   extends: './packages/cli/src/configs/vite.calender.base.ts',
+  //   test: {
+  //     // ...
+  //     include: ['tests/**/*.{browser}.test.{ts,js}'],
+  //     // 在使用内联配置的时候，建议定义一个名称
+  //     name: 'w-calender-test',
+  //     environment: 'w-calender-test',
+  //   },
+  // },
+  // {
+  //   test: {
+  //     include: ['tests/**/*.{node}.test.{ts,js}'],
+  //     name: 'node',
+  //     environment: 'node',
+  //   },
+  // },
 ]);
