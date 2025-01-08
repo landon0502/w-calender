56a460fe27ced29e3e9f3d0cc7fcb75607358e4a
wang huan <whuan0322@163.com>
2025-01-08 12:22:30 +0800

    feat: optimize

--- packages/calender/__test__/bus.test.ts
+++ packages/calender/__test__/bus.test.ts
@@ -17,6 +17,17 @@ busTest('test bus $emit and $on', ({ bus, key }) => {
   expect(num).toBe(10);
 });
 
+busTest('test bus parameter cache', ({ bus }) => {
+  let key = Symbol('test');
+  let v = 1;
+  const testFunc = (value: number) => {
+    v = value;
+  };
+  bus.$emit(key, 15);
+  bus.$on(key, testFunc, true);
+  expect(v).toBe(15);
+});
+
 busTest('test bus $has', ({ key, bus }) => {
   expect(bus.$has(key, testFunc)).toBe(true);
 });
@@ -37,13 +48,15 @@ busTest('test empty params of bus $off', ({ key, bus }) => {
 });
 
 busTest('test bus $once', ({ bus }) => {
+  let num = 0;
   function testFunc1(v: number) {
     num = v;
   }
   let key = Symbol('once');
   bus.$once(key, testFunc1);
-
+  bus.$emit(key, 200);
   expect(bus.$has(key, testFunc1)).toBe(false);
+  expect(num).toBe(200);
 });
 
 busTest('test bus $clear', ({ key, bus }) => {
--- packages/calender/src/store/Store.ts
+++ packages/calender/src/store/Store.ts
@@ -1,11 +1,21 @@
-import { createStore } from './';
-export default class Store {
-  state = null;
+import { isUndef } from '@/utils';
 
-  constructor() {}
+// import { createStore } from './';
+export class Store<T> {
+  state: T | Record<string, any> = {};
+
+  constructor(initialState: T | undefined) {
+    if (!isUndef(initialState)) {
+      this.state = initialState;
+    }
+  }
   getState() {}
   setState() {}
   removeState() {}
-  dispatch() {}
   clear() {}
+}
+
+export function createStore<State>(initialState: State) {
+  let store = new Store(initialState);
+  return store;
 }
