bd0c543a47b062e66960d85a5ad5a0acf1a0ee81
wang huan <whuan0322@163.com>
2025-02-09 20:38:10 +0800

    pref: Optimize

--- packages/calender/src/views/Week/index.tsx
+++ packages/calender/src/views/Week/index.tsx
@@ -2,6 +2,9 @@ import MultipleColumns from '@/components/MultipleColumns';
 import { WeekViewProps } from '@/types/components';
 import { getDaysByTimes, getTimeStartAndEnd } from '@/utils';
 export default function (props: WeekViewProps) {
+  console.log(
+    getTimeStartAndEnd(props.date, 'week').map(({ time }) => time.format('YYYY-MM-DD HH:mm'))
+  );
   return (
     <MultipleColumns {...props} days={getDaysByTimes(...getTimeStartAndEnd(props.date, 'week'))} />
   );
