82e5ab01a5fe54abd15303a37a3748305e12bc93
wang huan <whuan0322@163.com>
2025-03-02 19:38:22 +0800

    pref: Optimize

--- packages/calender/src/components/DragoverBubble/types.ts
+++ packages/calender/src/components/DragoverBubble/types.ts
@@ -1,4 +1,3 @@
-// 事件key
 import type { CalenderItem } from '@/types/options';
 import type { Rect, OperateType } from '@/types/components';
 export type DragConfig = { rect: Rect; data: CalenderItem; type: OperateType } | null;
